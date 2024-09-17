b24127e943c1a507fe3a4c63391fd19aa952b2db
zadort <zadort@kkszki.hu>
2024-09-17 11:27:47 +0200

    6. feladat kesz

--- Employers/Employers/Program.cs
+++ Employers/Employers/Program.cs
@@ -12,18 +12,53 @@ namespace Employers
     {
         static void Main(string[] args)
         {
+            #region 2. feladat
             List<Class1> lista = new List<Class1>();
+            #endregion
+
+                
+            #region 1. feladat
             var sorok = File.ReadAllLines("tulajdonsagok_100sor.txt");
+            #endregion
 
             foreach (var sor in sorok)
             {
                 lista.Add(new Class1(sor));
             }
 
+            #region 3.feladat
             foreach (var item in lista)
             {
-                Console.WriteLine($"{item.azonosito} {item.nev} {item.kor} {item.kereset}");
+                Console.WriteLine($"{item.nev}");
             }
+            #endregion
+
+            #region 4. feladat
+            int legnagyobbKereset = 0;
+            foreach (var item in lista)
+            {
+                if (legnagyobbKereset >)
+                {
+
+                }
+            }
+            #endregion
+
+            #region 5. feladat
+
+            #endregion
+
+            #region 6. feladat
+            int db = 0;
+            foreach (var item in lista)
+            {
+                if (item.kereset >= 50000)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine(db);
+            #endregion
 
             Console.ReadLine();
         }
