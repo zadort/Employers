9038cc8216221fea1ecef94c8d94d53975c2714e
zadort <zadort@kkszki.hu>
2024-09-17 12:13:44 +0200

    hiba javitasok

--- Employers/Employers/Program.cs
+++ Employers/Employers/Program.cs
@@ -27,25 +27,34 @@ namespace Employers
             }
 
             #region 3.feladat
+            Console.WriteLine("3. feladat");
+            Console.WriteLine("ÖSSZES NÉV");
             foreach (var item in lista)
             {
-                Console.WriteLine($"{item.nev}");
+                Console.Write($"{item.nev} ");
             }
+            Console.WriteLine();
+            Console.WriteLine();
             #endregion
 
             #region 4. feladat
-            int legnagyobbKereset = 0;
-            foreach (var item in lista)
+            Console.WriteLine("4. feladat");
+            Console.WriteLine("DOLGOZÓK AKIK A LEGJOBBAN KERESNEK");
+            Class1 legnagyobbKereset = lista[0];
+            for (int i = 0; i < lista.Count; i++)
             {
-                if (item.kereset > legnagyobbKereset)
+                if (lista[i].kereset > legnagyobbKereset.kereset)
                 {
-                    legnagyobbKereset = item.kereset;
-                }
-                Console.WriteLine($"{item.nev} {item.azonosito}" + legnagyobbKereset);
+                    legnagyobbKereset = lista[i];
+                }    
             }
+            Console.WriteLine($"{legnagyobbKereset.azonosito} {legnagyobbKereset.nev}");
+            Console.WriteLine();
             #endregion
 
             #region 5. feladat
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("AKIKNEK 10 ÉVÜK VAN A NYUGDÍJIG");
             foreach (var item in lista)
             {
                 if (65 - 10 == item.kor)
@@ -53,9 +62,12 @@ namespace Employers
                     Console.WriteLine($"{item.nev} {item.kor}");
                 }
             }
+            Console.WriteLine();
             #endregion
 
             #region 6. feladat
+            Console.WriteLine("6. feladat");
+            Console.WriteLine("HÁNYAN KERESNEK 50000FT FELETT");
             int db = 0;
             foreach (var item in lista)
             {
