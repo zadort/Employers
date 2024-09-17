d739c2171d81215a8663a3b114910756363cfd0a
zadort <zadort@kkszki.hu>
2024-09-17 11:16:47 +0200

    1-3 feladat

---
+++ Employers/Employers/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employers
+{
+    internal class Class1
+    {
+        public int azonosito;
+        public string nev;
+        public int kor;
+        public int kereset;
+
+        public Class1(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            this.azonosito = Convert.ToInt32(darabok[0]);
+            this.nev = darabok[1];
+            this.kor = Convert.ToInt32(darabok[2]);
+            this.kereset = Convert.ToInt32(darabok[3]);
+        }
+    }
+}
--- Employers/Employers/Program.cs
+++ Employers/Employers/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
 
 namespace Employers
 {
@@ -10,6 +12,20 @@ namespace Employers
     {
         static void Main(string[] args)
         {
+            List<Class1> lista = new List<Class1>();
+            var sorok = File.ReadAllLines("tulajdonsagok_100sor.txt");
+
+            foreach (var sor in sorok)
+            {
+                lista.Add(new Class1(sor));
+            }
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"{item.azonosito} {item.nev} {item.kor} {item.kereset}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
